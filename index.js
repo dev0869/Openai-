4590d05b852f06e2317d72f43c33ccdf71be090f
dev0869 <108985813+dev0869@users.noreply.github.com>
2023-03-10 07:16:39 -0500

    cors policy changed

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import PostRoutes from "./Routes/PostRoutes.js";
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"*",
+    credentials:true
+}))
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/post',ApiRoutes)
